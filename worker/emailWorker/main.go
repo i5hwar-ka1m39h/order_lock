e27fdf09f7ee6d2091069b68c0c2cb4c12eb3c9c
ishwar_kalmegh <ishwarkalmegh156@gmail.com>
2026-02-16 21:41:19 +0530

    worker base

--- worker/emailWorker/main.go
+++ worker/emailWorker/main.go
@@ -1,4 +1,49 @@
-package emailworker
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+)
+
+
+
+func main(){
+	fmt.Println("starting the email worker...")
+
+
+	err :=godotenv.Load()
+	if err != nil {
+		log.Fatalln("error loading the envs", err)
+		return
+	}
+
+	service_port := os.Getenv("SERVICE_PORT")
+	if service_port == ""{
+		log.Fatalln("service port is empty")
+		return
+	}
+
+	r := chi.NewRouter()
+	
+	server := &http.Server{
+		Handler: r,
+		Addr: ":" + service_port,
+	}
+
+
+	err = server.ListenAndServe()
+	if err != nil {
+		log.Fatalln("error while creating service worker", err)
+		return
+	}
+
+	fmt.Println("service worker is listening on port ", service_port, "...")
+}
 
 
 
